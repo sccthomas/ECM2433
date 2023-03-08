4eaf0cf78a0fea45086ab82b5aaed2df250989a4
sccthomas <95318179+sccthomas@users.noreply.github.com>
2023-03-08 19:26:56 +0000

    Question2 done quality average

--- Question2/riffle.c
+++ Question2/riffle.c
@@ -2,9 +2,12 @@
 
 
 int main(){
+
     /*
-    int numbers[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+    int numbers[] =  {0 ,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                      12, 13, 14, 15, 16, 17, 18, 19, 20};
     int pack_size = sizeof(numbers) / sizeof (numbers[0]);
+    printf("%d\n",pack_size);
     int *work = (int*)malloc(pack_size * sizeof (int ));
 
     riffle_once(numbers, pack_size, sizeof (int), work);
@@ -15,30 +18,30 @@ int main(){
     free(work);
      */
 
-
-
-
     /*
+    int pack_size;
     char *greek[] = {"alpha", "beta", "gamma", "delta", "epsilon", "zeta",
-                     "eta", "theta", "iota", "kappa", "lambda", "mu" };
-    int pack_size = sizeof(greek) / sizeof (greek[0]);
-    riffle(greek,pack_size,sizeof (greek[0]),1);
+                     "eta", "theta", "iota", "kappa", "lambda", "mu","fr" };
+    pack_size = sizeof(greek) / sizeof (greek[0]);
+    riffle(greek,pack_size,sizeof (greek[0]),7);
     int i;
     for (i = 0; i < pack_size; ++i) {
         printf("%s \n",greek[i]);
     }
-     */
 
-    /*
-    int numbers[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-    int pack_size = sizeof(numbers) / sizeof (numbers[0]);
+
+    int numbers[] = {0 ,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                     12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+                     23, 24, 25, 26, 27, 28, 29,
+                     30, 31, 32, 33, 34, 35, 36, 37, 38 ,39 ,
+                     40, 41, 42, 43 ,44 ,45 ,46 ,47 ,48 ,49, 50};
+    pack_size = sizeof(numbers) / sizeof (numbers[0]);
     riffle(numbers,pack_size,sizeof (numbers[0]),1);
-    int i;
     for (i = 0; i < pack_size; ++i) {
         printf("%d \n",numbers[i]);
     }
-    printf("%f \n",quality(numbers,pack_size));
-     */
+    //printf("%f \n",quality(numbers,pack_size));
+    */
 
     /*
     char *greek[] = {"alpha", "beta", "gamma", "delta", "epsilon",
@@ -47,20 +50,29 @@ int main(){
     int good = check_shuffle(greek,pack_size,sizeof (greek[0]),cmp_string);
     printf("%d \n",good);
     return 1;
-    */
-
+     */
 
     /*
     int numbers[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
     int pack_size = sizeof(numbers) / sizeof (numbers[0]);
     int good = check_shuffle(numbers,pack_size,sizeof (numbers[0]),cmp_integer);
-    printf("%d",good);
+    printf("%d \n",good);
     return 1;
      */
-    printf("%f",average_quality(1000,2,30));
+
+    //int b = quality_c();
+    //printf("%d \n",b);
+
+    //printf("final = %f\n",average_quality(50,1,3));
+    //int numbers[] = {0, 1, 2, 3, 4, 5, 6};
+    //int pack_size = sizeof(numbers) / sizeof (numbers[0]);
+    //int i;
+    //printf("%f \n",quality(numbers,pack_size));
+    //printf("%f \n",average_quality(50,12,30));
+    quality_c();
 
 }
-//////////////
+/////////////////////////////////
 
 void riffle(void *L, int len, int size, int N){
     int i;
@@ -76,24 +88,14 @@ void riffle_once(void *L, int len, int size, void *work){
     int i;
     int j;
     int k;
-    void *deck1_start;
-    void *deck2_start;
+    void *deck1_start = L + (size*(len/2));
+    void *deck2_start = L + (size*(len-1));
     void *deck1_end = L;
-    void *deck2_end;
+    void *deck2_end = deck1_start + size;
     int random;
     srand(time(0));
-
-    for (i = 0; i < len/2-1; ++i) {
-        L+=size;
-    }
-    deck1_start = L;
-    L+=size;
-    deck2_end = L;
-    L-=size;
-    for (i = 0; i < len/2; ++i) {
-        L+=size;
-    }
-    deck2_start = L;
+    //printf("d1 = %p d1end %p\n",deck1_start,deck1_end);
+    //printf("d2 = %p d2end %p\n",deck2_start,deck2_end);
     for (j = 0; j < len; ++j) {
         random = rand() % 2;
         if(random == 0){
@@ -135,69 +137,108 @@ void riffle_once(void *L, int len, int size, void *work){
     }
 }
 
-//////////////
+/////////////////////////////////
 
 int check_shuffle(void *L, int len, int size, int (*cmp)(void *, void *)){
-    // Call riffle and then make a copy of the previous L and then compare the two things.
-    // We need 2 or 3 functions to compare int to int and string to string, maybe char to char.
-    // The cmp is a function pointer to the compare function.
     int i;
     void *first;
     void *second;
-    void *copy_L = malloc(len * size); // This is the copy of L
+    void *copy_L = malloc(len * size); // This is the copy of L before shuffle
     memcpy(copy_L,L,(size*len));
+    riffle(L,len,size,1); // Shuffle the pack
 
-    riffle(L,len,size,1);
+    // Checking if all items in the original are in the shuffled array
+    void *copy_of_shuffle = malloc(len * size);
+    memcpy(copy_of_shuffle, L, (size*len));
+    qsort(copy_of_shuffle, len, size, (int (*)(const void *, const void *)) cmp);
+
+    void *copy_L_sort = malloc(len * size);
+    memcpy(copy_L_sort, copy_L, (size*len));
+    qsort(copy_L_sort, len, size, (int (*)(const void *, const void *)) cmp);
+
+    if((arrays_equal(copy_L_sort,copy_of_shuffle,len,size,cmp))!=1){ // if they are not equal
+        free(copy_of_shuffle);
+        free(copy_L_sort);
+        free(copy_L);
+        return 0;
+    } else{
+        free(copy_of_shuffle);
+        free(copy_L_sort);
+    }
+
+    // Check that at least one of the items has been moved to a new place.
+    // and that a shuffle has actually occurred
     for (i = 0; i< len; i++) {
         first = L + (i*size);
         second = copy_L + (i*size);
-        if(cmp(first,second)==1){
+        if(cmp(first,second)!=0){
             free(copy_L);
-            return 0;
+            return 1; // Follows rules
         }
     }
     free(copy_L);
-    return 1;
+    return 0; // Does not follow rules
+}
+
+int arrays_equal(void *copy_of_shuffle, void *copy_L_sort, int len, int size, int (*cmp)(void *, void *)){
+    int i;
+    void *first;
+    void *second;
+    for (i = 0; i< len; i++) {
+        first = copy_of_shuffle + (i*size);
+        second = copy_L_sort + (i*size);
+        if(cmp(first,second)!=0){
+            return 0; // they are not equal
+        }
+    }
+    return 1; // they are equal
 }
 
 int cmp_string(void *one, void *two){
     char *one_string = *((char **)one);
     char *two_string = *((char **)two);
-    printf("%s  vs %s \n",one_string, two_string);
+    //printf("%s  vs %s \n",one_string, two_string);
     if(strcmp(one_string,two_string)==0){
-        return 1;
+        return 0; // the two strings are equal to each other
     }else{
-        return 0;
+        if(strcmp(one_string,two_string)>0){
+            return 1; // one is greater than two
+        } else{
+            return -1; // two is greater than one
+        }
     }
 }
 
 int cmp_integer(void *one, void *two){
     int one_int = *((int*) one);
     int two_int = *((int*) two);
-    printf("%d  vs %d \n",one_int, two_int);
     if(one_int==two_int){
-        return 1;
-    }else{
         return 0;
+    }else{
+        if(one_int>two_int){
+            return 1;
+        } else{
+            return -1;
+        }
     }
 }
 
-//////////////
+/////////////////////////////////
 
 float quality(int *numbers, int len){
-    int greater_count = 0;
+    float greater_count = 0;
     float numbers_quality;
     int i;
     int current;
     int next;
-    for (i = 0; i < len-1; ++i) {
+    for (i = 0; i < len; ++i) {
         current = numbers[i];
         next = numbers[i+1];
         if(next>current){
             greater_count+=1;
         }
     }
-    numbers_quality = (float)greater_count/(float)len;
+    numbers_quality = (float)(greater_count/(len-1));
     return numbers_quality;
 }
 
@@ -207,37 +248,45 @@ float average_quality(int N, int shuffles, int trials){
     int i;
     int j;
     int *numbers = (int *)malloc(N*sizeof(int));
-    for(i = 0; i<=N-1; i++)
+    for(i = 0; i<N; i++)
     {
         numbers[i] = i;
     }
     for (j = 0; j < trials; ++j) {
         riffle(numbers,N,sizeof (int),shuffles);
+        for (i = 0; i < N; ++i) {
+            printf("%d ",numbers[i]);
+        }
+        printf("\n");
         quality_check = quality(numbers,N);
         numbers_average_quality += quality_check;
     }
-    numbers_average_quality = numbers_average_quality/(float)N;
+    numbers_average_quality = numbers_average_quality/(float)(trials);
     free(numbers);
     return numbers_average_quality;
 }
 
+int quality_c(){
+    // find which average is the best using 1-15 shuffles.
+    float average;
+    int best_N;
+    float best_Average = 0;
+    int i;
+    for (i = 1; i < 16; ++i) {
+        average = average_quality(50,i,30);
+        printf("average = %f     best = %f \n",average,best_Average);
+        if(average>best_Average){
+            best_Average = average;
+            best_N = i;
+            printf("%d \n",best_N);
+        }
+    }
+    printf("%d",best_N);
+    return best_N;
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/////////////////////////////////
 
 
 
